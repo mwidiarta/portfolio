e9d4f77615bf36ed65dbf3ed102ba5b789b0c768
madejunior123 <madewidi185@gmail.com>
2025-09-28 12:46:14 +0800

    Update Dark Mode

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Portfolio
+
+## About Me
+
+Hallo, I'm Widi.
+This is my Portfolio, you can see with to click the Live Preview below
+
+## Live Preview
+
+[View Live Preview][https://mwidiarta.github.io/portfolio/]
--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = this.document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
 
@@ -18,3 +23,32 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  darkToggle.checked
+    ? (html.classList.add("dark"), (localStorage.theme = "dark"))
+    : (html.classList.remove("dark"), (localStorage.theme = "light"));
+});
+
+// Switch Toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
